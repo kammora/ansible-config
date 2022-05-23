ff6435f7a472fe076a45f846e0b3034340e9d5c3
kammora <kammora@a.a>
2022-05-23 21:11:36 +0200

    updates

--- k8s/control-pane.sh
+++ k8s/control-pane.sh
@@ -14,5 +14,9 @@ sudo bash ./containerd.sh
 sudo bash ./kubeadm.sh
 
 # --service-cidr string     Default: "10.96.0.0/12"
-kubeadm init \
---apiserver-bind-port 8080 --apiserver-advertise-address localhost
+sudo kubeadm init \
+--apiserver-bind-port 8080 --apiserver-advertise-address 0.0.0.0
+
+mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
