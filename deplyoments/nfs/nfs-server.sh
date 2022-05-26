5bb4b913c2abc3d12b33056910dafe6943e32df6
kammora <kammora@a.a>
2022-05-26 14:13:05 +0200

    up

--- deplyoments/nfs/nfs-server.sh
+++ deplyoments/nfs/nfs-server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# https://www.digitalocean.com/community/tutorials/how-to-set-up-an-nfs-mount-on-ubuntu-20-04
+
+
 shareFolderPath="/share"
 
 sudo apt update
@@ -9,7 +12,11 @@ sudo apt install nfs-kernel-server
 sudo systemctl start nfs-kernel-server.service
 
 sudo mkdir -p $shareFolderPath
+sudo chown nobody:nogroup $shareFolderPath
 
-echo "$shareFolderPath *(rw,sync,subtree_check)" | sudo tee -a /etc/export
+#echo "$shareFolderPath *(rw,sync,all_squash)" | sudo tee -a /etc/export
+echo "$shareFolderPath *(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
 
 sudo exportfs -a
+sudo systemctl restart nfs-kernel-server.service
+sudo ufw disable # TODO
