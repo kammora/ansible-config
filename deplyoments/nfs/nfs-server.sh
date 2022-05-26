301489e7056e26391d61c4b06872063536771616
kammora <kammora@a.a>
2022-05-26 13:33:06 +0200

    bug fix

--- deplyoments/nfs/nfs-server.sh
+++ deplyoments/nfs/nfs-server.sh
@@ -10,6 +10,6 @@ sudo systemctl start nfs-kernel-server.service
 
 sudo mkdir -p $shareFolderPath
 
-cat "$shareFolderPath *(rw,sync,subtree_check)" | sudo tee -a /etc/export
+echo "$shareFolderPath *(rw,sync,subtree_check)" | sudo tee -a /etc/export
 
 sudo exportfs -a
